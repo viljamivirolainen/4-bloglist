199c626cee4e269d0e3da50dcb130d82554fbba1
Viljami Virolainen <viljami.virolainen@aalto.fi>
2019-09-16 23:09:21 +0300

    4.2. sovellus jaettu osiin ja toimii.

--- index.js
+++ index.js
@@ -1,46 +1,8 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoPwd = process.env.PASSWORD
-const mongoUrl = 'mongodb+srv://bloglistuser:'+ mongoPwd +'@cluster0-anxxp.mongodb.net/test?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
---
+++ models/blog.js
@@ -0,0 +1,12 @@
+//models/blog.js
+const mongoose = require('mongoose')
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+  
+  const Blog = mongoose.model('Blog', blogSchema)
+
+  module.exports = Blog
